002d02e1be8065c1901f9bcb02749fa25e15ca53
junqili <junqili259@gmail.com>
2021-02-07 17:34:51 -0500

    Optimize header for platform

--- informd/src/screens/NewsStackScreen.js
+++ informd/src/screens/NewsStackScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import NewsScreen from './NewsScreen'
 import ArticleDetailScreen from './ArticleDetailScreen'
 
 import DrawerMenu from '../components/DrawerMenu'
